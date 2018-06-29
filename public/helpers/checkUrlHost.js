ff86740441ad2d028f8435511dbc26f3ac94e418
Srivatsan Muralidharan <srivatsan.muralidharan@grainger.com>
2018-06-29 13:51:25 -0500

    Bug fix for company rendering

--- public/helpers/checkUrlHost.js
+++ public/helpers/checkUrlHost.js
@@ -1,6 +1,6 @@
 module.exports = function checkUrlHost(request) {
   var requestForCompany = 'akcs';
-  const incomingHost = request.hostname.split('.')[0];
+  const incomingHost = request.hostname.split('.')[1];
   console.log(incomingHost, request.hostname);
   if (incomingHost=== process.env.AKCS_HOST) {
     requestForCompany='AKCS';
